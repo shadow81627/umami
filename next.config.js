086c7ae1a316ef1553d8de8d047ac732c63aa43c
Damien Robinson <damien.robinson@pocketpasta.com>
2024-01-16 11:05:58 +1000

    fix: move cors headers

--- next.config.js
+++ next.config.js
@@ -23,15 +23,6 @@ const headers = [
       .join(';')
       .replace(/\s{2,}/g, ' ')
       .trim(),
-  },
-  {
-    source: "/api/:path*",
-    headers: [
-      { key: "Access-Control-Allow-Credentials", value: "true" },
-      { key: "Access-Control-Allow-Origin", value: "*" },
-      { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-      { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-    ]
   }
 ];
 
@@ -135,6 +126,15 @@ const config = {
         source: '/:path*',
         headers,
       },
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
     ];
   },
   async rewrites() {
